b6812124f95b9f33717d5ffeaead31ff5aa0d225
Kiyori <113906780+thxforall@users.noreply.github.com>
2024-07-28 23:11:59 +0900

    fetch data

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,10 +2,16 @@ export const metadata = {
   title: 'Home',
 };
 
-export default function Page() {
-  return (
-    <>
-      <h1>Hello World!!</h1>
-    </>
-  );
+const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 10000));
+  const response = await fetch(URL);
+  const json = await response.json();
+  return json;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies)}</div>;
 }
